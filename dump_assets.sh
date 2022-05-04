1cedd0a168f7ca31f1b2ee68580e49314eda7d98
ksaweryr <ksawery.radziwilowicz@gmail.com>
2022-05-04 12:29:49 +0200

    Refactor CMakeLists.txt and dump_assets.sh

--- dump_assets.sh
+++ dump_assets.sh
@@ -1,13 +1,26 @@
 #!/bin/bash
 
-for path in assets/gfx/*; do
-    source_file=${path##*/}
-    object=${source_file%.*}
-    target_file="$1/${object}.cpp"
-    temp_file=$(mktemp --suffix='.bmp')
-    xxd_var=$(echo ${temp_file} | sed 's/\W/_/g')
+path=$1
+target_dir=$2
+source_file=${path##*/}
+object=${source_file%.*}
+target_file="${target_dir}/${object}.cpp"
 
-    montage ${path} -tile x1 -alpha On -geometry '+0+0' -background 'rgba(0, 0, 0, 0)' -quality 100 ${temp_file}
-    xxd -i ${temp_file} ${target_file}
-    sed -i "s/${xxd_var}/${object}/g" ${target_file}
-done
+subdir=$(echo ${path} | sed -r 's/^.*\/(.+)\/.*$/\1/')
+case $subdir in
+    "gfx")
+        temp_file=$(mktemp --suffix='.bmp')
+        xxd_var=$(echo ${temp_file} | sed 's/\W/_/g')
+
+        montage ${path} -tile x1 -alpha On -geometry '+0+0' -background 'rgba(0, 0, 0, 0)' -quality 100 ${temp_file}
+        xxd -i ${temp_file} ${target_file}
+        sed -i "s/${xxd_var}/${object}/g" ${target_file}
+        ;;
+    "fonts")
+        xxd -i ${path} ${target_file}
+        ;;
+    *)
+        echo "Unknown subdirectory ${subdir}!"
+        exit 1
+        ;;
+esac
